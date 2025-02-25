a587b35d712a88477749589dbf7cdd016316177f
Bhavanam Reddy <rbhavanam@mnipdnxrbhavanam1.modeln.com>
2025-02-25 07:13:14 -0800

    working

--- app.py
+++ app.py
@@ -4,17 +4,13 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def home():
-    return "<h1>for the l1 review</h1>"
+    return "<h1>doing the l1 review</h1>"
 
 @app.route("/webhook", methods=['POST'])
 def webhook():
     if request.method == 'POST':
         data = request.json
-        if data and 'pull_request' in data:
-            pr_info = data['pull_request']
-            print(f"PR Title: {pr_info['title']}")
-            print(f"PR URL: {pr_info['html_url']}")
-            print(f"PR User: {pr_info['user']['login']}")
+        print(data)  # Print the raw JSON payload to the console
         return jsonify({'status': 'success'}), 200
     return jsonify({'status': 'failure'}), 400
 
